2472b6d17985b8a88b2538eb683376769493573d
Kenny Lee <hotelfish@gmail.com>
2019-03-11 13:22:52 +0800

    large and larger button 1

--- test0311/test0311/Form1.Designer.cs
+++ test0311/test0311/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(58, 34);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(274, 83);
+            this.button1.Size = new System.Drawing.Size(178, 85);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
